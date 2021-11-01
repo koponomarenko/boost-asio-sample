77e8475b9638db4a28e6de74e91dc585b9842fc8
Kostiantyn Ponomarenko <kp@koponomarenko.com>
2021-11-01 23:46:39 +0200

    prepare tcp connection

--- client/main.cpp
+++ client/main.cpp
@@ -5,17 +5,32 @@
 using namespace boost;
 
 int main()
+try {
+    asio::ip::address server_ip_addr = asio::ip::address::from_string("127.0.0.1");
+    unsigned short port_num = 33'333;
+    asio::ip::tcp::endpoint server_ep {server_ip_addr, port_num};
+
+    asio::io_context io_context;
+    asio::ip::tcp::socket socket {io_context, server_ep.protocol()};
+    // socket.bind(local_endpoint) is done implicitly by socket.connect()
+    socket.connect(server_ep);
+}
+catch (std::exception& e) {
+    std::cerr << "std::exception: " << e.what() << std::endl;
+}
+catch (...) {
+    std::cerr << "Unknown exception" << std::endl;
+}
+
+// just a sample
+auto resolve_dns_name()
 {
-    std::string raw_ip_addr = "127.0.0.1";
-    unsigned short port_num = 3333;
-
-    boost::system::error_code ec;
-    asio::ip::address ip_addr = asio::ip::address::from_string(raw_ip_addr, ec);
-    if (ec.value() != 0) {
-        std::cout << "Failed to parse the IP address. Error code = " << ec.value()
-                  << ". Message: " << ec.message();
-        return ec.value();
+    std::string hostname = "localhost";
+    std::string port_num = "33333";
+    asio::io_context io_context;
+    asio::ip::tcp::resolver resolver {io_context};
+    auto results = resolver.resolve(hostname, port_num);
+    for (asio::ip::tcp::endpoint const& endpoint : results) {
+        std::cout << endpoint << std::endl;
     }
-
-    asio::ip::tcp::endpoint ep {ip_addr, port_num};
 }
--- server/main.cpp
+++ server/main.cpp
@@ -5,9 +5,24 @@
 using namespace boost;
 
 int main()
-{
-    asio::ip::address ip_addr = asio::ip::address_v6::any();
-    unsigned short port_num = 3333;
-
+try {
+    asio::ip::address ip_addr = asio::ip::address_v4::any();
+    unsigned short port_num = 33'333;
     asio::ip::tcp::endpoint ep {ip_addr, port_num};
+
+    asio::io_context io_context;
+    asio::ip::tcp::acceptor acceptor {io_context, ep.protocol()};
+    acceptor.bind(ep);
+
+    const int pending_requests_queue_size = 50;
+    acceptor.listen(pending_requests_queue_size);
+
+    asio::ip::tcp::socket socket {io_context};
+    acceptor.accept(socket);
+}
+catch (std::exception& e) {
+    std::cerr << "std::exception: " << e.what() << std::endl;
+}
+catch (...) {
+    std::cerr << "Unknown exception" << std::endl;
 }
