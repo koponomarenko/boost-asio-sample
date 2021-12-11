daedbf136bc05acfd39b3cee416a68839f06f397
Kostiantyn Ponomarenko <kp@koponomarenko.com>
2021-12-11 15:20:19 +0200

    async send and receive msg

--- client/main.cpp
+++ client/main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <memory>
+#include <string>
 
 #include <boost/asio.hpp>
 
 using namespace boost;
+
+struct Session {
+    std::shared_ptr<asio::ip::tcp::socket> sock;
+    std::string buf;
+};
+
+// this is invoked when all data is writen or an error occurred.
+void callback(const boost::system::error_code& ec,
+              std::size_t bytes_transferred,
+              std::shared_ptr<Session> session)
+{
+    if (ec.value() != 0) {
+        std::cerr << "Error in callback. Error code value = " << ec.value()
+                  << ". Message: " << ec.message() << std::endl;
+        return;
+    }
+}
 
 int main()
 try {
@@ -11,9 +30,18 @@ try {
     asio::ip::tcp::endpoint server_ep {server_ip_addr, port_num};
 
     asio::io_context io_context;
-    asio::ip::tcp::socket socket {io_context, server_ep.protocol()};
+    std::shared_ptr<asio::ip::tcp::socket> socket {
+        std::make_shared<asio::ip::tcp::socket>(io_context, server_ep.protocol())};
     // socket.bind(local_endpoint) is done implicitly by socket.connect()
-    socket.connect(server_ep);
+    socket->connect(server_ep);
+
+    std::string msg {"Hello there!"};
+    std::shared_ptr<Session> session {std::make_shared<Session>()};
+    session->sock = socket;
+    session->buf = msg;
+
+    asio::async_write(*socket, asio::buffer(session->buf),
+                      std::bind(callback, std::placeholders::_1, std::placeholders::_2, session));
 }
 catch (std::exception& e) {
     std::cerr << "std::exception: " << e.what() << std::endl;
@@ -22,6 +50,7 @@ catch (...) {
     std::cerr << "Unknown exception" << std::endl;
 }
 
+// =====================================================================
 // just a sample
 auto resolve_dns_name()
 {
--- server/main.cpp
+++ server/main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <vector>
 
 #include <boost/asio.hpp>
 
 using namespace boost;
+
+struct Session {
+    std::shared_ptr<asio::ip::tcp::socket> sock;
+    std::vector<char> buf;
+    const int buf_size {100};
+};
+
+// this is invoked when all data is writen or an error occurred.
+void handler(const boost::system::error_code& ec,
+             std::size_t bytes_transferred,
+             std::shared_ptr<Session> session)
+{
+    if (ec.value() != 0) {
+        std::cerr << "Error in callback. Error code value = " << ec.value()
+                  << ". Message: " << ec.message() << std::endl;
+        return;
+    }
+
+    std::string msg {session->buf.begin(), session->buf.begin() + bytes_transferred};
+    std::cout << "received msg: " << msg << std::endl;
+}
+
+void log(const std::string& s) {
+    std::cout << "log: " << s << std::endl;
+}
 
 int main()
 try {
@@ -17,8 +43,26 @@ try {
     const int pending_requests_queue_size = 50;
     acceptor.listen(pending_requests_queue_size);
 
-    asio::ip::tcp::socket socket {io_context};
-    acceptor.accept(socket);
+    asio::io_context io_context2; // tmp
+    auto socket = std::make_shared<asio::ip::tcp::socket>(io_context);
+    log("wait for the client");
+    acceptor.accept(*socket);
+    log("client connected");
+
+    auto session {std::make_shared<Session>()};
+    session->sock = socket;
+    session->buf.resize(session->buf_size, '\0');
+
+    log("do async_read");
+    asio::async_read(*socket, asio::buffer(session->buf, 11),
+                     std::bind(handler, std::placeholders::_1, std::placeholders::_2, session));
+
+    //    char buf2[100] = {};
+    //    socket.read_some(asio::buffer(buf2, 20));
+    //    std::cout << "received msg: " << buf2 << std::endl;
+    io_context.run();
+    log("wait for async_read");
+    std::this_thread::sleep_for(std::chrono::seconds {10});
 }
 catch (std::exception& e) {
     std::cerr << "std::exception: " << e.what() << std::endl;
