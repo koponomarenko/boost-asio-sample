b023e099b9df71a410735bfa91fded7a70d5097f
Kostiantyn Ponomarenko <kp@koponomarenko.com>
2021-12-12 23:02:26 +0200

    refactor

--- client/main.cpp
+++ client/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <memory>
 #include <string>
@@ -6,50 +7,65 @@
 
 using namespace boost;
 
-void log(const std::string& s) { std::cout << "log: " << s << std::endl; }
+// hardcode fixed msg size for simplicity
+constexpr int msg_size = 100;
+
+void log(const std::string& s)
+{
+    std::cout << "log: " << s << std::endl;
+}
 
 struct Session {
-    std::shared_ptr<asio::ip::tcp::socket> sock;
-    std::string buf;
+    std::shared_ptr<asio::ip::tcp::socket> socket;
+    std::vector<char> buffer;
 };
 
-// this is invoked when all data is writen or an error occurred.
-// another possible name: write_handler()
-void callback(const boost::system::error_code& ec,
-              std::size_t bytes_transferred,
-              std::shared_ptr<Session> session)
+// this is invoked when all data is writen or an error occurs
+void async_write_handler(const boost::system::error_code& ec,
+                         std::size_t bytes_transferred,
+                         std::shared_ptr<Session> session)
 {
     if (ec.value() != 0) {
-        std::cerr << "Error in callback. Error code value = " << ec.value()
+        std::cerr << "ERROR: in async_write_handler. Error code " << ec.value()
                   << ". Message: " << ec.message() << std::endl;
         return;
     }
     log("msg was sent");
 }
 
+std::vector<char> transform_to_msg(const std::string& s)
+{
+    std::vector<char> msg;
+    msg.resize(msg_size, '\0');
+    int len = (s.size() < msg_size) ? s.size() : msg_size;
+    std::copy(s.begin(), s.begin() + len, msg.begin());
+    return msg;
+}
+
 int main()
 try {
-    asio::ip::address server_ip_addr = asio::ip::address::from_string("127.0.0.1");
-    unsigned short port_num = 33'333;
-    asio::ip::tcp::endpoint server_ep {server_ip_addr, port_num};
+    asio::ip::address server_ip_addr{asio::ip::address::from_string("127.0.0.1")};
+    unsigned short port_num{33'333};
+
+    asio::ip::tcp::endpoint server_ep{server_ip_addr, port_num};
 
     asio::io_context io_context;
-    std::shared_ptr<asio::ip::tcp::socket> socket {
-        std::make_shared<asio::ip::tcp::socket>(io_context, server_ep.protocol())};
+    auto socket{std::make_shared<asio::ip::tcp::socket>(io_context, server_ep.protocol())};
     // socket.bind(local_endpoint) is done implicitly by socket.connect()
     socket->connect(server_ep);
 
-    std::string msg {"Hello there!"};
-    std::shared_ptr<Session> session {std::make_shared<Session>()};
-    session->sock = socket;
-    session->buf = msg;
+    std::string text{"Hello there! It actually works! =)"};
+    auto session{std::make_shared<Session>()};
+    session->socket = socket;
+    session->buffer = transform_to_msg(text);
 
-    log("do async_read");
-    asio::async_write(*socket, asio::buffer(session->buf),
-                      std::bind(callback, std::placeholders::_1, std::placeholders::_2, session));
+    log("do async_write");
+    asio::async_write(
+        *socket, asio::buffer(session->buffer, msg_size),
+        std::bind(async_write_handler, std::placeholders::_1, std::placeholders::_2, session));
 
     log("wait for async_write finish");
-    // handlers are (waited for and) called in call;
+    // all handlers are (waited for and) called in this call
     io_context.run();
 }
 catch (std::exception& e) {
@@ -66,7 +82,7 @@ auto resolve_dns_name()
     std::string hostname = "localhost";
     std::string port_num = "33333";
     asio::io_context io_context;
-    asio::ip::tcp::resolver resolver {io_context};
+    asio::ip::tcp::resolver resolver{io_context};
     auto results = resolver.resolve(hostname, port_num);
     for (asio::ip::tcp::endpoint const& endpoint : results) {
         std::cout << endpoint << std::endl;
--- server/main.cpp
+++ server/main.cpp
@@ -5,57 +5,64 @@
 
 using namespace boost;
 
+// hardcode fixed msg size for simplicity
+constexpr int msg_size = 100;
+
+void log(const std::string& s)
+{
+    std::cout << "log: " << s << std::endl;
+}
+
 struct Session {
-    std::shared_ptr<asio::ip::tcp::socket> sock;
-    std::vector<char> buf;
-    const int buf_size {100};
+    std::shared_ptr<asio::ip::tcp::socket> socket;
+    std::vector<char> buffer;
 };
 
-// this is invoked when all data is writen or an error occurred.
-void handler(const boost::system::error_code& ec,
-             std::size_t bytes_transferred,
-             std::shared_ptr<Session> session)
+// this is invoked when all data is writen or an error occurs
+void async_read_handler(const boost::system::error_code& ec,
+                        std::size_t bytes_transferred,
+                        std::shared_ptr<Session> session)
 {
     if (ec.value() != 0) {
-        std::cerr << "Error in callback. Error code value = " << ec.value()
+        std::cerr << "ERROR: in async_read_handler. Error code " << ec.value()
                   << ". Message: " << ec.message() << std::endl;
         return;
     }
 
-    std::string msg {session->buf.begin(), session->buf.begin() + bytes_transferred};
+    std::string msg{session->buffer.begin(), session->buffer.end()};
     std::cout << "received msg: " << msg << std::endl;
 }
 
-void log(const std::string& s) { std::cout << "log: " << s << std::endl; }
-
 int main()
 try {
-    asio::ip::address ip_addr = asio::ip::address_v4::any();
-    unsigned short port_num = 33'333;
-    asio::ip::tcp::endpoint ep {ip_addr, port_num};
+    asio::ip::address ip_addr{asio::ip::address_v4::any()};
+    unsigned short port_num{33'333};
+
+    asio::ip::tcp::endpoint ep{ip_addr, port_num};
 
     asio::io_context io_context;
-    asio::ip::tcp::acceptor acceptor {io_context, ep.protocol()};
+    asio::ip::tcp::acceptor acceptor{io_context, ep.protocol()};
     acceptor.bind(ep);
 
-    const int pending_requests_queue_size = 50;
+    const int pending_requests_queue_size{50};
     acceptor.listen(pending_requests_queue_size);
 
     auto socket = std::make_shared<asio::ip::tcp::socket>(io_context);
     log("wait for the client");
     acceptor.accept(*socket);
-    log("client connected");
 
-    auto session {std::make_shared<Session>()};
-    session->sock = socket;
-    session->buf.resize(session->buf_size, '\0');
+    log("client connected");
+    auto session{std::make_shared<Session>()};
+    session->socket = socket;
+    session->buffer.resize(msg_size, 0);
 
     log("do async_read");
-    asio::async_read(*socket, asio::buffer(session->buf, 11),
-                     std::bind(handler, std::placeholders::_1, std::placeholders::_2, session));
+    asio::async_read(
+        *socket, asio::buffer(session->buffer, msg_size),
+        std::bind(async_read_handler, std::placeholders::_1, std::placeholders::_2, session));
 
     log("wait for async_read finish");
-    // handlers are (waited for and) called in call;
+    // all handlers are (waited for and) called in this call
     io_context.run();
 }
 catch (std::exception& e) {
